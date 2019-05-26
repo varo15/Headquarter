53b4cf283e16837f7ece0a6709151d8d5cf42933
varog <varog74@gmail.com>
2019-05-26 19:39:27 +0200

    Mostrar por pantalla toda la informacion del usuario

--- app/src/main/java/com/headquarter/com/headquarter/activity/activity/fragment/ProfileFragment.java
+++ app/src/main/java/com/headquarter/com/headquarter/activity/activity/fragment/ProfileFragment.java
@@ -8,6 +8,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
@@ -22,6 +23,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.headquarter.R;
 import com.headquarter.com.headquarter.activity.activity.BottomNavigationViewActivity;
+import com.headquarter.com.headquarter.activity.activity.ConnectionDB;
 import com.headquarter.com.headquarter.activity.activity.LogInActivity;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Transformation;
@@ -35,16 +37,26 @@ import java.sql.Statement;
  */
 public class ProfileFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
+    //Variables usuario firebase
     private FirebaseAuth firebaseAuth;
     private FirebaseUser user;
+    //Variables datos del usuario
     private TextView userEmail;
     private TextView userName;
     private TextView userDNI;
+    private TextView userBirthDate;
+    private TextView userPhone;
+    private TextView userTeam;
+    private TextView userFAANumber;
     private ImageView userImage;
+
+
     private Button buttonLogOut;
     private View view;
+    private ResultSet resultSet;
     private ProgressBar progressBar;
-    private SwipeRefreshLayout swipeRefreshLayout;
+
+    //Variable consulta sql
     private String sql;
 
 
@@ -59,8 +71,10 @@ public class ProfileFragment extends Fragment implements SwipeRefreshLayout.OnRe
 
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
-        sql = "SELECT * FROM `jugador` WHERE idGoogle = '4kl2hv7YvFUPJ7qpxixcovtKrVx2' ";
-        System.out.println(sql);
+        sql = "SELECT * FROM `jugador` WHERE idGoogle = '" + user.getUid() + "'";
+
+        //
+
         new ProfileTask().execute();
 
     }
@@ -68,18 +82,10 @@ public class ProfileFragment extends Fragment implements SwipeRefreshLayout.OnRe
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        swipeRefreshLayout = view.findViewById(R.id.swiperefresh);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                showUserData();
-            }
-        });
-
         buttonLogOut = view.findViewById(R.id.buttonLogOut);
-
         buttonLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -94,21 +100,48 @@ public class ProfileFragment extends Fragment implements SwipeRefreshLayout.OnRe
 
             }
         });
-        showUserData();
 
         return view;
     }
 
     public void showUserData() {
 
-        userImage = view.findViewById(R.id.userImage);
-        Picasso.get().load(user.getPhotoUrl()).resize(500, 500).transform(new CircleTransform()).into(userImage);
+        try {
+            userImage = view.findViewById(R.id.userImage);
+            Picasso.get().load(user.getPhotoUrl()).resize(500, 500).transform(new CircleTransform()).into(userImage);
 
-        userEmail = view.findViewById(R.id.userEmail);
-        userEmail.setText(user.getEmail());
+            userEmail = view.findViewById(R.id.userEmail);
+            userEmail.setVisibility(View.VISIBLE);
+            userEmail.setText(user.getEmail());
 
-        userName = view.findViewById(R.id.userName);
-        userName.setText(user.getDisplayName());
+            userName = view.findViewById(R.id.userName);
+            userName.setVisibility(View.VISIBLE);
+            userName.setText(user.getDisplayName());
+
+            userDNI = view.findViewById(R.id.userDNI);
+            userDNI.setVisibility(View.VISIBLE);
+            userDNI.setText(resultSet.getString("DNI"));
+
+            userBirthDate = view.findViewById(R.id.userDate);
+            userBirthDate.setVisibility(View.VISIBLE);
+            userBirthDate.setText(resultSet.getString("fecha_nacimiento"));
+
+            userPhone = view.findViewById(R.id.userPhone);
+            userPhone.setVisibility(View.VISIBLE);
+            userPhone.setText(resultSet.getString("telefono"));
+
+            userTeam = view.findViewById(R.id.userTeam);
+            userTeam.setVisibility(View.VISIBLE);
+            userTeam.setText(resultSet.getString("id_equipo_fk"));
+
+            userFAANumber = view.findViewById(R.id.userFAANumber);
+            userFAANumber.setVisibility(View.VISIBLE);
+            userFAANumber.setText(resultSet.getString("numero_FAA"));
+
+
+        } catch (Exception e) {
+
+        }
     }
 
     @Override
@@ -157,16 +190,17 @@ public class ProfileFragment extends Fragment implements SwipeRefreshLayout.OnRe
     private class ProfileTask extends AsyncTask {
 
         @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
         protected Object doInBackground(Object[] objects) {
+
             try {
                 Statement statement = BottomNavigationViewActivity.connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(sql);
+                resultSet = statement.executeQuery(sql);
                 resultSet.next();
-                System.out.println(resultSet.getString("DNI"));
-                userDNI = view.findViewById(R.id.userDNI);
-                userDNI.setText(resultSet.getString("DNI"));
 
-                BottomNavigationViewActivity.connection.close();
 
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -178,6 +212,14 @@ public class ProfileFragment extends Fragment implements SwipeRefreshLayout.OnRe
         @Override
         protected void onPostExecute(Object o) {
 
+
+            try {
+                showUserData();
+                BottomNavigationViewActivity.connection.close();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
 
         }
     }
