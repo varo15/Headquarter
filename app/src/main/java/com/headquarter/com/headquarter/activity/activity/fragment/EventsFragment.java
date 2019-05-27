3ef02e406973a43e23d843a91b801f2bd161af41
varog <varog74@gmail.com>
2019-05-27 18:13:45 +0200

    Cambiar el modelo xml y aniadir el swipeToRefresh

--- app/src/main/java/com/headquarter/com/headquarter/activity/activity/fragment/EventsFragment.java
+++ app/src/main/java/com/headquarter/com/headquarter/activity/activity/fragment/EventsFragment.java
@@ -1,28 +1,17 @@
 package com.headquarter.com.headquarter.activity.activity.fragment;
 
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.headquarter.R;
-import com.headquarter.com.headquarter.activity.activity.adapter.EventsFragmentAdapter;
-import com.headquarter.com.headquarter.activity.activity.BottomNavigationViewActivity;
+import com.headquarter.com.headquarter.activity.activity.AdapterRecycler;
 
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -30,37 +19,11 @@ import java.util.ArrayList;
  */
 public class EventsFragment extends Fragment {
 
-    //Variables usuario firebase
-    private FirebaseAuth firebaseAuth;
-    private FirebaseUser user;
-
     ArrayList<String> listDatos;
     RecyclerView recycler;
-    private ResultSet resultSet;
-    private String sql;
-    private ProgressBar progressBar;
-    private SwipeRefreshLayout swipeRefreshLayout;
-
-    //Array donde se guardan los datos
-    ArrayList<ArrayList> listOfEvents = new ArrayList<ArrayList>();
-
 
     public EventsFragment() {
         // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-
-        //Lamamos al emtodo para obtener el usuario y preparar la consulta
-        getUser();
-        //Preparamos la consulta con el uui de nuestro usuario logeado
-        sql = "SELECT `partida`.*, `campo`.`nombreCampo`FROM `partida`" +
-                "LEFT JOIN `campo` ON `partida`.`id_campo_fk` = `campo`.`idCampo`";
-
-        //Ejecutar la tarea que devulve la consulta
-        new EventsTask().execute();
-        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -68,82 +31,20 @@ public class EventsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View view = inflater.inflate(R.layout.fragment_events, container, false);
-        recycler = view.findViewById(R.id.recycler);
+        View rootView = inflater.inflate(R.layout.fragment_events, container, false);
+        recycler = rootView.findViewById(R.id.recycler);
         recycler.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        //ProgressBar
-        progressBar = view.findViewById(R.id.progressBar);
-        progressBar.setVisibility(View.VISIBLE);
+        listDatos = new ArrayList<String>();
 
-        // Refresh
-        swipeRefreshLayout = view.findViewById(R.id.pullToRefresh);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                new EventsTask().execute();
-            }
-        });
-
-        return view;
-    }
-
-    private class EventsTask extends AsyncTask {
-
-        @Override
-        protected Object doInBackground(Object[] objects) {
-
-
-            try {
-                listOfEvents.clear();
-                Statement statement = BottomNavigationViewActivity.connection.createStatement();
-                resultSet = statement.executeQuery(sql);
-                ResultSetMetaData rsm = resultSet.getMetaData();
-
-                int columnsNumber = rsm.getColumnCount();
-                int i;
-
-                resultSet.beforeFirst();
-
-                while (resultSet.next()) {
-                    //Cremos un array nuevo por cada fila de la consulta y lo guardamos en listOfEvents
-                    ArrayList<String> event = new ArrayList<String>();
-                    for (i = 1; i <= columnsNumber; i++) {
-                        event.add(resultSet.getString(i));
-                    }
-                    listOfEvents.add(event);
-                }
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-            return null;
+        for(int i = 0;i<50;i++){
+            listDatos.add("Dato "+i);
         }
 
-        @Override
-        protected void onPostExecute(Object o) {
-            progressBar.setVisibility(View.GONE);
-            loadEventsCards();
-            swipeRefreshLayout.setRefreshing(false);
-
-
-        }
-    }
-
-    private void loadEventsCards() {
-        //En este metodo ira todo el codigo necesario para que se carguen los datos y se dibujen los cardviews, antes de que se dibujen se mostrara el fragment en blanco con el progresbar dando vueltas
-        //Una vez que carguen, el progressbar se desactiva y se pintan las tarjetas
-
-        EventsFragmentAdapter adapter = new EventsFragmentAdapter(listOfEvents);
+        AdapterRecycler adapter = new AdapterRecycler(listDatos);
         recycler.setAdapter(adapter);
+
+        return rootView;
     }
 
-    /*
-        Metodo que nos devuelve el usuario de firebase
-     */
-    private void getUser() {
-        firebaseAuth = FirebaseAuth.getInstance();
-        user = firebaseAuth.getCurrentUser();
-    }
 }
--- app/src/main/java/com/headquarter/com/headquarter/activity/activity/fragment/EventsRegisteredFragment.java
+++ app/src/main/java/com/headquarter/com/headquarter/activity/activity/fragment/EventsRegisteredFragment.java
@@ -5,22 +5,17 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.headquarter.R;
-import com.headquarter.com.headquarter.activity.activity.adapter.EventsRegisteredFragmentAdapter;
+import com.headquarter.com.headquarter.activity.activity.AdapterRecycler;
 import com.headquarter.com.headquarter.activity.activity.BottomNavigationViewActivity;
 
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -30,19 +25,10 @@ import java.util.ArrayList;
  */
 public class EventsRegisteredFragment extends Fragment {
 
-    //Variables usuario firebase
-    private FirebaseAuth firebaseAuth;
-    private FirebaseUser user;
-
     ArrayList<String> listDatos;
     RecyclerView recycler;
     private ResultSet resultSet;
     private String sql;
-    private ProgressBar progressBar;
-    private SwipeRefreshLayout swipeRefreshLayout;
-
-    //ArrayList necesarios para la carga de datos
-    static ArrayList<ArrayList> getListOfEventsRegistered = new ArrayList<ArrayList>();
 
     public EventsRegisteredFragment() {
         // Required empty public constructor
@@ -51,44 +37,30 @@ public class EventsRegisteredFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
 
-        //Lamamos al emtodo para obtener el usuario y preparar la consulta
-        getUser();
-        //Preparamos la consulta con el uui de nuestro usuario logeado
-        sql = "SELECT `partida`.*, `participa`.`idGoogle_fk`, `campo`.`nombreCampo`FROM `partida`" +
-                "LEFT JOIN `participa` ON `participa`.`idPartida_fk` = `partida`.`idPartida`" +
-                "LEFT JOIN `campo` ON `partida`.`id_campo_fk` = `campo`.`idCampo`" +
-                "WHERE `participa`.`idGoogle_fk` = '" + user.getUid() + "'";
-
-        //Ejecutar la tarea que devulve la consulta
-        new EventsRegisteredTask().execute();
-        super.onCreate(savedInstanceState);
+                super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_events_registered, container, false);
-        recycler = view.findViewById(R.id.recycler);
+        View rootView = inflater.inflate(R.layout.fragment_events_registered, container, false);
+        recycler = rootView.findViewById(R.id.recycler);
         recycler.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        listDatos = new ArrayList<String>();
 
-        //ProgressBar
-        progressBar = view.findViewById(R.id.progressBar);
-        progressBar.setVisibility(View.VISIBLE);
+        for (int i = 0; i < 10; i++) {
+            listDatos.add("Dato " + i);
+        }
 
-        // Refresh
-        swipeRefreshLayout = view.findViewById(R.id.pullToRefresh);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                new EventsRegisteredTask().execute();
-            }
-        });
-        return view;
+        AdapterRecycler adapter = new AdapterRecycler(listDatos);
+        recycler.setAdapter(adapter);
+
+        return rootView;
     }
 
-    private class EventsRegisteredTask extends AsyncTask {
+    private class ProfileTask extends AsyncTask {
 
 
         @Override
@@ -96,25 +68,10 @@ public class EventsRegisteredFragment extends Fragment {
 
 
             try {
-                getListOfEventsRegistered.clear();
+
                 Statement statement = BottomNavigationViewActivity.connection.createStatement();
                 resultSet = statement.executeQuery(sql);
-                ResultSetMetaData rsm = resultSet.getMetaData();
-                resultSet.beforeFirst();
-
-                int columsNumber = rsm.getColumnCount();
-                int i;
-
-                while (resultSet.next()) {
-                    //Cremos un array nuevo por cada fila de la consulta y lo guardamos en listOfEvents
-                    ArrayList<String> event = new ArrayList<String>();
-                    for (i = 1; i <= columsNumber; i++) {
-                        event.add(resultSet.getString(i));
-                    }
-                    getListOfEventsRegistered.add(event);
-                    System.out.println(getListOfEventsRegistered);
-                }
-
+                resultSet.next();
 
 
             } catch (SQLException e) {
@@ -127,28 +84,10 @@ public class EventsRegisteredFragment extends Fragment {
 
         @Override
         protected void onPostExecute(Object o) {
-            progressBar.setVisibility(View.GONE);
-            loadEventsRegisteredCards();
-            swipeRefreshLayout.setRefreshing(false);
+
 
 
         }
-    }
-
-    private void loadEventsRegisteredCards() {
-        //En este metodo ira todo el codigo necesario para que se carguen los datos y se dibujen los cardviews, antes de que se dibujen se mostrara el fragment en blanco con el progresbar dando vueltas
-        //Una vez que carguen, el progressbar se desactiva y se pintan las tarjetas
-
-        EventsRegisteredFragmentAdapter adapter = new EventsRegisteredFragmentAdapter(getListOfEventsRegistered);
-        recycler.setAdapter(adapter);
-    }
-
-    /*
-        Metodo que nos devuelve el usuario de firebase
-     */
-    private void getUser() {
-        firebaseAuth = FirebaseAuth.getInstance();
-        user = firebaseAuth.getCurrentUser();
     }
 
 }
