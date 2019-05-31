5fa656f39db8f94a7082baf5b62bcf1604f15726
Dondorito <38208232+Dondorito@users.noreply.github.com>
2019-06-01 00:23:55 +0200

    Checkbox de registro

--- app/src/main/java/com/headquarter/com/headquarter/activity/activity/activity/RegisterActivity.java
+++ app/src/main/java/com/headquarter/com/headquarter/activity/activity/activity/RegisterActivity.java
@@ -2,14 +2,35 @@ package com.headquarter.com.headquarter.activity.activity.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
 import com.headquarter.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    CheckBox checkbox;
+    EditText numero;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        checkbox = findViewById(R.id.checkBox);
+        numero = findViewById(R.id.editNumero);
+
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(checkbox.isChecked()){
+                    numero.setVisibility(View.VISIBLE);
+                }
+                else{
+                    numero.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
