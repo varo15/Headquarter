132f9e819a5f2a9b9c6498afed3ed0708d728bd4
Dondorito <38208232+Dondorito@users.noreply.github.com>
2019-06-01 14:11:12 +0200

    SplashScreen añadida

---
+++ app/src/main/java/com/headquarter/com/headquarter/activity/activity/activity/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.headquarter.com.headquarter.activity.activity.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.headquarter.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
