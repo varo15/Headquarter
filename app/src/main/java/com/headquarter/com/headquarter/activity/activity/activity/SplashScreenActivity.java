d526925c5e900c77e1501ba36e1b3a358d6b1709
varog <varog74@gmail.com>
2019-06-01 16:12:50 +0200

    Ocultar botones virtuales en SplashScreen

--- app/src/main/java/com/headquarter/com/headquarter/activity/activity/activity/SplashScreenActivity.java
+++ app/src/main/java/com/headquarter/com/headquarter/activity/activity/activity/SplashScreenActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,9 +19,11 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+
         new ConnectionDB().execute();
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
+        hideVirtualButtons();
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -40,4 +43,14 @@ public class SplashScreenActivity extends AppCompatActivity {
 
 
     }
+
+    private void hideVirtualButtons() {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
 }
